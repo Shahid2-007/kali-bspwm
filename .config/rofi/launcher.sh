04c76b71dad9a0fb79cf9a6ff41d147c521d6e27
Shahid2-007 <500booksinkindle@gmail.com>
2026-01-05 13:01:52 +0530

    Fourth commit

--- .config/rofi/launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Detect if rofi is already running
-if pgrep -x "rofi" >/dev/null; then
-  pkill -x rofi
-else
-  rofi -no-config -show drun -modi drun -theme ~/.config/rofi/launcher.rasi
-fi
